3e65b6641e9a10185eefb3d93bdbe334e4d8ac60
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-11-10 15:55:00 +0800

    Added migrations for passenger.

--- MagisAir/settings.py
+++ MagisAir/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'bootstrap5'
+	'bootstrap5',
+    'User_Management'
 ]
 
 MIDDLEWARE = [
