684baab1b10595b01d44a988e6d8fd1abf2c2c50
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-11-10 17:18:00 +0800

    Added signup and update views for Passenger.

--- User_Management/views.py
+++ User_Management/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
-# Create your views here.
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.models import User
+
+from .forms import SignupForm, PassengerUpdateForm
+from .models import Passenger
+
+class SignupView(CreateView):
+    """A signup view for a new user. """
+    model = User
+    form_class = SignupForm
+    template_name = 'signup.html'
+    success_url = reverse('home')
+    
+    def form_valid(self, form):
+        """ auto-login once signed up. """
+        user = form.save()
+        login(self.request, user) 
+        return redirect(self.success_url)
+  
+class PassengerUpdateView(LoginRequiredMixin, UpdateView):
+    """ An update view for passenger information. """
+    model = Passenger
+    form_class = PassengerUpdateForm
+    template_name = 'passenger_update.html'
+    
+    def get_success_url(self):
+        return reverse('home')
