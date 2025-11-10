3b681abe9c58096463d05a40ac3b09c278ee9e7f
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-11-10 15:23:24 +0800

    Added user model for passenger.

--- User_Management/models.py
+++ User_Management/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Passenger(models.Model):
+    """ A user model for passenger. """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254, blank=True)
+    first_name = models.CharField(max_length=50, blank=True)
+    middle_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    birthdate = models.DateField()
+    gender = models.CharField(max_length=1, choices=[('F', 'Female'), ('M', 'Male')]), default='M', blank=False, null=False)
+    
+    def __str__(self):
+        return f'{str(self.last_name)}, {str(self.first_name)} {str(self.middle_name)[0]}.'
+    
+    def get_absolute_url(self):
+        return reverse('passenger', args=[self.pk])
+        #return reverse('passenger')
+
