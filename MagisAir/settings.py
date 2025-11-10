2eda88af9afccaed20e6268288a9709a3a899c1a
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-11-10 16:43:26 +0800

    Added static folder and templates.

--- MagisAir/settings.py
+++ MagisAir/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'MagisAir.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'MagisAir/templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'MagisAir/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
