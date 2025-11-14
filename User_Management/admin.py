c9def247e74ceaec6929a7ca0351b3eec196f55d
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-11-14 19:14:21 +0800

    Removed inlines of Passenger under User.

--- User_Management/admin.py
+++ User_Management/admin.py
@@ -4,15 +4,6 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import Passenger
 
-class PassengerInline(admin.StackedInline):
-    """ Admin inline fields for passenger. """
-    model = Passenger
-    can_delete = False
-    
-class UserAdmin(BaseUserAdmin):
-    """ Admin panel for passenger. """
-    inlines = [PassengerInline,]
-    
-#admin.site.register(Passenger)
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.register(Passenger)
+
+
