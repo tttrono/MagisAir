ccdfe46d62cbf5a9c2bd1d6c3443bdbbae7ea1ae
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-11-22 19:42:35 +0800

    Renamed User_Management to Passenger.

--- User_Management/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class UserManagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'User_Management'
-    
-    def ready(self):
-        """Configure signals for user management. """
-        import User_Management.signals
--- User_Management/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('signup/', SignupView.as_view(), name='signup'),
-    path('<int:pk>/', PassengerInfoView.as_view(), name='update'),
-]
-
-app_name = 'User_Management'
